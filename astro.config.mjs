1fa408b91dffb5bfa77a66feee91878462be886c
Abiral Tamang <abiral.tamang816@gmail.com>
2024-05-16 10:48:01 +0545

    documentation

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig, passthroughImageService } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    starlight({
+      title: "KathCode",
+      social: {
+        github: "https://github.com/Kathford-IT-Club-KIC",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          link: "/introduction",
+          items: [],
+        },
+        {
+          label: "Event Timeline",
+          items: [{ label: "Timeline", link: "/event-timeline/timeline/" }],
+        },
+        {
+          label: "Rulebook",
+          items: [
+            { label: "Kathcode Rulebook", link: "/rules/rules/" },
+            { label: "Initial Selection Criteria", link: "/rules/selection/" },
+            { label: "Evaulation Criteria", link: "/rules/evaulate/" },
+          ],
+        },
+        {
+          label: "Registration",
+          items: [
+            { label: "How to register?", link: "/registration/register/" },
+          ],
+        },
+        {
+          label: "Sponsor",
+          items: [{ label: "Sponsorship", link: "/sponsorship/sponsor/" }],
+        },
+      ],
+    }),
+  ],
+});
